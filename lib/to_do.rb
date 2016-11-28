bf62cb6ad93e73401b63876de99df37375b9005b
Jonathan Thom and Tracie Weitzman <jonathan@>
2016-11-28 15:15:29 -0800

    make to-do list from epicodus lesson

---
+++ lib/to_do.rb
@@ -0,0 +1,23 @@
+class Task
+  @@all_tasks = []
+
+  define_method(:initialize) do |description|
+    @description = description
+  end
+
+  define_method(:description) do
+    @description
+  end
+
+  define_singleton_method(:all) do
+    @@all_tasks
+  end
+
+  define_method(:save) do
+    @@all_tasks.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_tasks = []
+  end
+end
